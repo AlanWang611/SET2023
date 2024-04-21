f7e5daec40b37e55acce70b4a7c3f58681788f31
anthony <anthonyjyao@gmail.com>
2024-04-21 13:14:28 -0400

    done

--- computer_vision/trash_detection.py
+++ computer_vision/trash_detection.py
@@ -1,11 +1,10 @@
-import ultralytics, cv2
 from ultralytics import YOLO
 from PIL import Image
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
 
-image = cv2.imread("frame.jpg")
+
+image_count = 0
+
 
 def predict_image(image_path):
     model = YOLO('best_trash.pt')
@@ -13,46 +12,37 @@ def predict_image(image_path):
     #               line_width=3)
     im = Image.open(image_path).convert('RGB')
     result = model(im, imgsz=1280, conf=0.15, show_labels=True, show_conf=True, iou=0.5, line_width=3)
-    print(result[0].tojson())
-    # result boxes: all coords: x, y, width, height
-    result_boxes = result[0].boxes.xywh.cpu().detach().numpy()
-    return result_boxes
-
-
-
-saved_ndarray = None
+    # result boxes: all coords: x1, y1, x2, y2
+    # result_boxes = result[0].boxes.xyxy.cpu().detach().numpy()
+    return result[0].tojson()
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     # POST method handler
     def do_POST(self):
-        global saved_ndarray
-
+        global image_count
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        image_filename = 'litter.jpg'
-
+        image_filename = 'images/' + str(image_count) + '.jpg'
+        image_count += 1
         with open(image_filename, 'wb') as f:
             f.write(post_data)
 
         prediction = predict_image(image_filename)
-        saved_ndarray = prediction
-        processed_result = prediction
 
-        # response_content = json.dumps(processed_result)
-        print(processed_result)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        # self.wfile.write(response_content.encode('utf-8'))
+        self.wfile.write(prediction.encode('utf-8'))
 
 
-# server address
-server_address = ('', 8000)
+def main():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, HTTPRequestHandler)
+    print('start server')
+    httpd.serve_forever()
 
-httpd = HTTPServer(server_address, HTTPRequestHandler)
 
-# start
-print('start server')
-httpd.serve_forever()
+if __name__ == "__main__":
+    main()
