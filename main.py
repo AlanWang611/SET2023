698d3d0668abfb35c4255e78cf909b464f8386c5
raspberrysir <141175376+raspberrysir@users.noreply.github.com>
2024-04-23 20:14:41 -0400

    edited main.py with tcp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import cv2
 import serial
+import socket
+import cv2
 
 """
 camera
@@ -8,22 +9,44 @@ sensors (lidar)
 navigation algorithm
 """
 
+# def main():
+#     ser = serial.Serial('/dev/ttyUSB0', 9600)
+#
+#     try:
+#         distance = True
+#         while True:
+#             data = ser.readline().decode().strip()
+#             if distance:
+#                 print("Distance:", data)
+#                 distance = not distance
+#             else:
+#                 print("Strength:", data)
+#                 distance = not distance
+#     except KeyboardInterrupt:
+#         ser.close()
+
+
+
+
+HOST = "10.138.148.113"
+PORT = 65432
 
 def main():
     ser = serial.Serial('/dev/ttyUSB0', 9600)
 
     try:
+        data = ser.readline().decode().strip()
         distance = True
-        while True:
-            data = ser.readline().decode().strip()
-            if distance:
-                print("Distance:", data)
-                distance = not distance
-            else:
-                print("Strength:", data)
-                distance = not distance
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            message = f"{data},{distance}"
+            s.sendall(message.encode())
+            distance = not distance
+            data = s.recv(1024)
     except KeyboardInterrupt:
         ser.close()
+
+    print(f"Received {data!r}")
 
 
 if __name__ == "__main__":
